d64f58035bb6581bad1ad9c6473eb39fffce4fb3
Vlad-Buko <vladislav_buko@mail.ru>
2022-11-04 23:53:07 +0300

    Added server part

---
+++ src/main/java/com/shopBack/ApplicationShop.java
@@ -0,0 +1,15 @@
+package com.shopBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Vladislav Domaniewski
+ */
+
+@SpringBootApplication
+public class ApplicationShop {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationShop.class, args);
+    }
+}
